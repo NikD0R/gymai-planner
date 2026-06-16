4d5b69b7696b96263a0212c3ba0a5f707eb9f4fc
Nikita Doroshenko <nikitokd05@gmail.com>
2026-06-17 02:47:26 +0300

    updated cors settings

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://gymai-planner.vercel.app"],
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 app.use(express.json());
 
